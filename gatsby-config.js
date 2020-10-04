d0bd49874fa723149377f5652455ff418f428c0c
justvr <justinavaldarajic@gmail.com>
2020-10-04 09:54:03 +0200

    :mag: changing manifest title

--- gatsby-config.js
+++ gatsby-config.js
@@ -27,8 +27,8 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `balkan bread`,
+        short_name: `balkan bread`,
         start_url: `/`,
         background_color: `#663399`,
         theme_color: `#663399`,
