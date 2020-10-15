132aa2813d7f7331caafd4d69a763cb096b851bb
justvr <justinavaldarajic@gmail.com>
2020-10-15 08:34:53 +0200

    Update robots to disallow

--- gatsby-config.js
+++ gatsby-config.js
@@ -10,7 +10,7 @@ module.exports = {
       options: {
         host: 'https://www.balkanbread.com',
         sitemap: 'https://www.balkanbread.com/sitemap.xml',
-        policy: [{ userAgent: '*', allow: '/' }]
+        policy: [{ userAgent: '*', disallow: '' }]
       }
     },
     `gatsby-plugin-sitemap`,
