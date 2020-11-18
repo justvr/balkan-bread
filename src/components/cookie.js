685a120e7bb6b645b00d88f559f7fbe98398e0e3
justvr <justinavaldarajic@gmail.com>
2020-11-18 20:58:13 +0100

    :recycle: cookie class to hooks
    class to hooks; cookie service

--- src/components/cookie.js
+++ src/components/cookie.js
@@ -1,106 +1,76 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import '../service/cookie.js'
+import cookie from '../service/cookie.js'
 import Link from 'gatsby-link'
 
-class Cookie  extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state = {show: true}
-    this.accept = this.accept.bind(this)
+function Cookie() {
+  const [show, setShow] = useState(true)
+
+  function accept() {
+    cookie.set('GA4', 'accepted')
+    setShow(false)
+  }
+  function reject() {
+    setShow(false)
   }
 
-  /**
-   * @see https://www.w3schools.com/js/js_cookies.asp
-   */
-  getCookie(cname) {
-    var name = cname + "=";
-    var decodedCookie = decodeURIComponent(document.cookie);
-    var ca = decodedCookie.split(';');
-    for(var i = 0; i <ca.length; i++) {
-      var c = ca[i];
-      while (c.charAt(0) === ' ') {
-        c = c.substring(1);
-      }
-      if (c.indexOf(name) === 0) {
-        return c.substring(name.length, c.length);
-      }
+  useEffect(() => {
+    if ('accepted' === cookie.get('GA4')) {
+      setShow(false)
     }
-    return "";
-  }
+  }, [])
 
-  componentDidMount() {
-    if ('true' === this.getCookie('GA4')) {
-      this.setState(() => ({
-        show: false
-      }))
-    }
-  }
-
-  accept() {
-    document.cookie = 'GA4=true'
-    this.setState(() => ({
-      show: false
-    }))
-  }
-
-  reject() {
-    this.setState(() => ({
-      show: false
-    }))
-  }
-
-  render() {
-    return this.state.show ? (
+  return show ? (
+    <div
+      style={{
+        background: '#fff4e1',
+        borderRadius: '9px',
+        bottom: '100px',
+        boxShadow: '0 3px 5px -1px rgba(0,0,0,.2), 0 6px 10px 0 rgba(0,0,0,.14), 0 1px 18px 0 rgba(0,0,0,.12)',
+        maxWidth: '320px',
+        padding: '1.0875rem',
+        position: 'fixed',
+        right: 0,
+        width: '100%',
+      }}
+    >
       <div
         style={{
-          background: '#fff4e1',
-          borderRadius: '9px',
-          bottom: '100px',
-          boxShadow: '0 3px 5px -1px rgba(0,0,0,.2), 0 6px 10px 0 rgba(0,0,0,.14), 0 1px 18px 0 rgba(0,0,0,.12)',
-          maxWidth: '320px',
-          padding: '1.0875rem',
-          position: 'fixed',
-          right: 0,
-          width: '100%',
+          margin: '0 auto',
+          maxWidth: 860,
         }}
       >
-        <div
+        <p>How about, cookies
+          <br />
+          <small>
+            We've got some cookies to improve your experience.
+            By using Balkan Bread you agree to our
+            <Link to="/privacy-and-cookie-management" style={{marginLeft: '6px'}}>
+              Cookie Policy
+            </Link>.
+          </small>
+        </p>
+        <button
+          onClick={accept}
           style={{
-            margin: '0 auto',
-            maxWidth: 860,
-          }}
-        >
-          <p>How about, cookies
-            <br />
-            <small>
-              We've got some cookies to improve your experience.
-              By using Balkan Bread you agree to our
-              <Link to="/privacy-and-cookie-management" style={{marginLeft: '6px'}}>
-                Cookie Policy
-              </Link>.
-            </small>
-          </p>
-          <button
-            onClick={this.accept}
-            style={{
-              border: 0,
-              padding: '0.4rem 1.2rem',
-              background: 'rgb(49, 127, 96)',
-              borderRadius: '30px',
-              color: 'white',
-              textTransform: 'uppercase',
-              fontSize: '0.8rem',
-              marginRight: '0.4rem',
-            }}>
-              got it
-            </button>
-          <button
-            style={{background: 'none', border: 0}}
-          >
-            reject
+            border: 0,
+            padding: '0.4rem 1.2rem',
+            background: 'rgb(49, 127, 96)',
+            borderRadius: '30px',
+            color: 'white',
+            textTransform: 'uppercase',
+            fontSize: '0.8rem',
+            marginRight: '0.4rem',
+          }}>
+            got it
           </button>
-        </div>
-      </div>) : null
-  }
+        <button
+          onClick={reject}
+          style={{background: 'none', border: 0}}
+        >
+          reject
+        </button>
+      </div>
+    </div>) : null
 }
-
-export default Cookie;
+export default Cookie
---
+++ src/service/cookie.js
@@ -0,0 +1,26 @@
+export default {
+  set(name, value) {
+    var expiryDate = new Date()
+    expiryDate.setMonth(expiryDate.getMonth() + 1)
+    console.log(expiryDate)
+    document.cookie = `${name}=${value}; expires=${expiryDate}`;
+  },
+  /**
+   * @see https://www.w3schools.com/js/js_cookies.asp
+   */
+  get(cookieName) {
+    var name = cookieName + "=";
+    var decodedCookie = decodeURIComponent(document.cookie);
+    var ca = decodedCookie.split(';');
+    for(var i = 0; i <ca.length; i++) {
+      var c = ca[i];
+      while (c.charAt(0) === ' ') {
+        c = c.substring(1);
+      }
+      if (c.indexOf(name) === 0) {
+        return c.substring(name.length, c.length);
+      }
+    }
+    return "";
+  }
+}
